30eb8476895893344d61e879603cda7ce136c911
jonathas-silva <silva.jonathas@outlook.com>
2024-06-22 11:51:16 -0300

    icrialÃção da tabela de gastos

--- app/src/main/java/com/example/domesticbudget/database/DatabaseHelper.kt
+++ app/src/main/java/com/example/domesticbudget/database/DatabaseHelper.kt
@@ -20,27 +20,51 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, NOME_BANCO_DA
         const val COLUNA_CATEGORIAS_NOME = "nome"
         const val COLUNA_CATEGORIAS_VALOR = "valor"
         const val COLUNA_CATEGORIAS_PERIODO = "periodo"
+
+        //colunas da tabela gastos
+        const val COLUNA_GASTOS_ID = "id"
+        const val COLUNA_GASTOS_VALOR = "valor"
+        const val COLUNA_GASTOS_DESCRICAO = "descricao"
+        const val COLUNA_GASTOS_CATEGORIA = "categoria"
+        const val COLUNA_GASTOS_DATA = "data"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val antigoSql =   "CREATE TABLE $NOME_TABELA_CATEGORIAS (\n" +
-                    "$COLUNA_CATEGORIAS_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
-                    "$COLUNA_CATEGORIAS_NOME VARCHAR(100) NOT NULL);"
+        val antigoSql = "CREATE TABLE $NOME_TABELA_CATEGORIAS (\n" +
+                "$COLUNA_CATEGORIAS_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "$COLUNA_CATEGORIAS_NOME VARCHAR(100) NOT NULL);"
 
-        val sql =   "CREATE TABLE $NOME_TABELA_CATEGORIAS (\n" +
-                    "$COLUNA_CATEGORIAS_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
-                    "$COLUNA_CATEGORIAS_NOME VARCHAR(100) NOT NULL,\n" +
-                    "$COLUNA_CATEGORIAS_VALOR NUMERIC(1000000,2) NOT NULL,\n" +
-                    "$COLUNA_CATEGORIAS_PERIODO VARCHAR(100)\n" +
-                    "  );"
+        val sqlTabela1 = "CREATE TABLE $NOME_TABELA_CATEGORIAS (\n" +
+                "$COLUNA_CATEGORIAS_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "$COLUNA_CATEGORIAS_NOME VARCHAR(100) NOT NULL,\n" +
+                "$COLUNA_CATEGORIAS_VALOR NUMERIC(1000000,2) NOT NULL,\n" +
+                "$COLUNA_CATEGORIAS_PERIODO VARCHAR(100)\n" +
+                "  );"
 
+        val sqlTabela2 = "CREATE TABLE $NOME_TABELA_GASTOS (\n" +
+                "$COLUNA_GASTOS_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n" +
+                "$COLUNA_GASTOS_VALOR REAL(1000000,2) NOT NULL DEFAULT 0,\n" +
+                "$COLUNA_GASTOS_DESCRICAO VARCHAR(300),\n" +
+                "$COLUNA_GASTOS_CATEGORIA INTEGER NOT NULL,\n" +
+                "$COLUNA_GASTOS_DATA VARCHAR(100),\n" +
+                "FOREIGN KEY($COLUNA_GASTOS_CATEGORIA) REFERENCES $NOME_TABELA_CATEGORIAS($COLUNA_CATEGORIAS_ID));"
+
+        /*Vamos fazer duas chamadas sequenciais para o execSQL, porque só podemos criar a tabela gastos
+        * se conseguirmos criar a tabela categorias, pela relação de FK*/
         try {
-            db?.execSQL(sql)
+            db?.execSQL(sqlTabela1)
             Log.i("info_db", "Sucesso ao criar tabela categorias!")
-        }catch (e: Exception){
+
+            try {
+                db?.execSQL(sqlTabela2)
+                Log.i("info_db", "Sucesso ao criar tabela gastos!")
+            } catch (e: Exception) {
+                Log.e("info_db", "Erro ao criar tabela gastos!!")
+            }
+
+        } catch (e: Exception) {
             Log.e("info_db", "Erro ao criar tabela categorias!!")
         }
-
 
     }
 
